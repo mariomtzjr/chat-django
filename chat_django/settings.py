05d9d839671d4fb367327091810ccec5bb864b77
mariomtzjr <francisco.smtz@hotmail.com>
2020-07-21 12:46:32 -0500

    Added channels layer.

--- chat_django/settings.py
+++ chat_django/settings.py
@@ -72,7 +72,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'chat_django.wsgi.application'
 # Channels
 ASGI_APPLICATION = 'chat_django.routing.application'
-
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
